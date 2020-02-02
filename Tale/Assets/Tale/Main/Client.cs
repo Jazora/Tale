78acc673db7d1792d696b67f2d03d968dd9da48f
Jazora <jason_weller@hotmail.co.uk>
2020-02-02 06:26:55 +0000

    update

---
+++ Tale/Assets/Tale/Main/Client.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using Steamworks;
+
+public class Client : MonoBehaviour
+{
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+
+        try
+        {
+            SteamClient.Init(480);
+        }
+        catch (System.Exception e)
+        {
+            Debug.Log(e);
+        }
+
+        // Print out some basic information
+        //Debug.Log("My Steam ID: " + SteamClient.SteamId);
+        //Debug.Log("My Steam Username: " + SteamClient.Name);
+    }
+
+    void Update()
+    {
+        if (SteamClient.IsLoggedOn)
+        {
+            SteamClient.RunCallbacks();
+        }
+    }
+
+    void OnDisable()
+    {
+        if (SteamClient.IsLoggedOn)
+        {
+            SteamClient.Shutdown();
+        }
+    }
+}
---
+++ Tale/Assets/Tale/Master/Master.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using Steamworks;
+
+public class Master : MonoBehaviour
+{
+
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+
+        SteamServerInit serverInit = new SteamServerInit("gmod", "Garry Mode")
+        {
+            GamePort = 28015,
+            Secure = true,
+            QueryPort = 28016,
+            DedicatedServer = false
+        };
+
+        try
+        {
+            SteamServer.Init(480, serverInit);
+        }
+        catch (System.Exception e)
+        {
+            Debug.Log(e);
+        }
+
+        SteamServer.OnSteamServersConnected += () =>
+        {
+            Debug.Log("Connected");
+        };
+    }
+}
